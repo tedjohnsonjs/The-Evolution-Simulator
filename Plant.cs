39ad2a8460270313c96b5453855e2430ad18e94a
Ted Johnson <33895577+tedjohnsonjs@users.noreply.github.com>
2018-02-03 08:49:57 +0000

    Create Plant.cs

---
+++ Plant.cs
@@ -0,0 +1,17 @@
+
+namespace The_Evolution_Simulator
+{
+    public class Plant
+    {
+        //=============
+        // Variables
+        public float posX, posY, size;
+
+        public Plant (float _x, float _y, float _size)
+        {
+            posX = _x;
+            posY = _y;
+            size = _size;
+        }
+    }
+}
